6d193cf953d777f4d799d1d48dec23a098648dc5
Brit <brit.finnegan@gmail.com>
2020-04-06 22:38:04 -0500

    updated CORS functionality

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,8 +1,22 @@
 exports.handler = function(event, context, callback) {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+  };
+
+  if (event.httpMethod !== "POST") {
+    return callback(null, {
+      statusCode: 200,
+      headers,
+      body: "This was not a POST request"
+    });
+  }
+
   const secretContent = `
-    <h3>Welcome to the Secret Area</h3>
-    <p>Here we can tell you that the sky is <strong>blue</strong> and two plus two equals four.</p>
-    `;
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `;
+
   let body;
 
   if (event.body) {
@@ -11,14 +25,16 @@ exports.handler = function(event, context, callback) {
     body = {};
   }
 
-  if (body.password == "javascriptPasswordPublicCanSee") {
+  if (body.password == "javascript") {
     callback(null, {
       statusCode: 200,
+      headers,
       body: secretContent
     });
   } else {
     callback(null, {
-      statusCode: 401
+      statusCode: 401,
+      headers
     });
   }
 };
